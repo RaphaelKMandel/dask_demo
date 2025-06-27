7f91fa7170b9d59f4d21a6bca851351e488f3104
Raphael Mandel <raphael.k.mandel@gmail.com>
2025-06-26 22:15:45 -0400

    add physics class to make it closer to real example

--- pool_approach.py
+++ pool_approach.py
@@ -2,21 +2,39 @@ from time import sleep
 from random import choice
 from multiprocessing import Pool
 
-def worker_task(x):
-    print("worker", x)
-    sleep(choice([0.1 * i for i in range(10)]))
-    return "this", "that", x * x
 
-def when_done(result):
-    print(f"Result received: {result}")
+class Physics:
+    def __init__(self, x):
+        self.x = x
+
+    def solve(self):
+        print("solving with", str(self.x))
+        return self.x + 1
+
+    def callback(self, result):
+        print("callback with ", str(result))
+        self.x = result
+
+
 
 if __name__ == "__main__":
+        physics = [Physics(i) for i in [1, 10, 100, 1000]]
         pool = Pool(processes=4)
         for i in range(10):
-            pool.apply_async(worker_task, args=(i,), callback=when_done)
-            # result = pool.apply(worker_task, args=(i,))
-            # when_done(result)
-            sleep(0.1)
+            results = []
+            for p in physics:
+                result = pool.apply_async(p.solve, callback=p.callback)
+                results.append(result)
+                # result = pool.apply(worker_task, args=(i,))
+                # when_done(result)
+                # sleep(0.1)
+
+            for r in results:
+                r.get()
+
+            print("\n\nNext Iteration\n\n")
+
+
 
         while True:
             pass
